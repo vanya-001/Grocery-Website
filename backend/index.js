25c096c86817364ecd739fd15d14dd9a267c4af8
Vanya Maheshwari <vanimaheshwari19@gmail.com>
2023-07-24 15:41:20 +0530

    products modified to frontend

--- backend/index.js
+++ backend/index.js
@@ -111,4 +111,10 @@ app.post("/uploadProduct", async(req, res) =>{
     res.send({message : "Added"})
 })
 
+// 
+app.get("/product", async(req, res) =>{
+    const data = await productModel.find({})
+    res.send(JSON.stringify(data))
+})
+
 app.listen(PORT, () => console.log("Server is running at port : " + PORT))
