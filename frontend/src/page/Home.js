38610d3d6593dcb0bb2404cdd16105aaceed1a58
Vanya Maheshwari <vanimaheshwari19@gmail.com>
2023-07-26 13:50:19 +0530

    Vegetable added on HomePage

--- frontend/src/page/Home.js
+++ frontend/src/page/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import HomeCart from '../component/HomeCart'
 import { useSelector } from 'react-redux'
+import CartFeature from '../component/CartFeature'
 
 const Home = () => {
   const productData = useSelector((state)=>state.product.productList)
   console.log(productData)
   const homeProductCartList = productData.slice(12,18)
+
+  const homeProductCartListVegetable = productData.filter(el => el.category === "vegetable")
+  console.log(homeProductCartListVegetable)
+
+  const loadingArray = new Array(4).fill(null)
 
   return (
     <div className='p-2 md:p-4'>
@@ -23,7 +29,7 @@ const Home = () => {
         </div>
         <div className='md:w-1/2 flex flex-wrap gap-5 p-4 justify-center'>
           {
-            homeProductCartList[0] && homeProductCartList.map(el =>{
+            homeProductCartList[0] ? homeProductCartList.map(el =>{
               return(
                 <HomeCart
                   key = {el._id}
@@ -34,11 +40,37 @@ const Home = () => {
                 />
               )
             })
+            :
+            loadingArray.map((el, index) => {
+              return(
+                <HomeCart 
+                  key = {index}
+                  loading = {"Loading..."}
+                />
+              )
+            })
           }
           
         </div>
-        <div className=''></div>
       </div>
+      <div className=''>
+          <h2 className='font-bold text-2xl text-green-700'>Fresh Vegetables</h2>
+          <div className='flex gap-5'>
+            {
+              homeProductCartListVegetable.map(el => {
+                return(
+                  <CartFeature
+                    key = {el._id}
+                    name = {el.name}
+                    category = {el.category}
+                    price = {el.price}
+                    image = {el.image}
+                  />
+                )
+              })
+            }
+          </div>
+        </div>
     </div>
   )
 }
