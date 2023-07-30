168a44d00878e5696d87eb4d6641ce7f54d045d3
Vanya Maheshwari <vanimaheshwari19@gmail.com>
2023-07-30 11:19:37 +0530

    Menu Page done

--- frontend/src/component/CartFeature.js
+++ frontend/src/component/CartFeature.js
@@ -1,25 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const CartFeature = ({image, name, price, category, loading}) => {
+const CartFeature = ({image, name, price, category, loading, id}) => {
   return (
     <div className='w-full min-w-[200px] max-w-[200px] bg-white hover:shadow-lg drop-shadow-lg px-4 pt-5 cursor-pointer flex flex-col'>
         {
             image ? <>
+            <Link to={`menu/${id}`} >
             <div className='h-28 flex flex-col justify-center items-center'>
                 <img src={image} className='h-full' />
             </div>
             <h3 className='font-semibold text-slate-600 capitalize text-lg mt-4'>{name}</h3>
             <p className=' text-slate-500 font-m'>{category}</p>
             <p className=' text-slate-700 font-bold'><span className='text-red-400'>₹</span>{price}</p>
-            <button className='bg-yellow-500 py-1 my-3 rounded'>Add Cart</button>
+            <button className='bg-yellow-500 py-1 my-3 rounded hover:bg-yellow-600 w-full'>Add Cart</button>
+            </Link>
             </>
 
             :
             <div className='min-h-[150px] flex justify-center items-center'>
                 <p>{loading}</p>
             </div>
+            
         }
     </div>
+    
   )
 }
 
--- frontend/src/page/Menu.js
+++ frontend/src/page/Menu.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
 
 const Menu = () => {
+  const {filterby} = useParams()
+  const productData = useSelector(state => state.product.productList)
+  
+  const productDisplay = productData.filter(el => el._id === filterby)[0]
+  console.log(productDisplay)
+
+  if (!productDisplay) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      
+    <div className='p-2 md:p-4'>
+      <div className='w-full max-w-3xl bg-white m-auto md:flex'>
+        <div className='max-w-sm overflow-hidden w-full p-5 '>
+          <img src={productDisplay.image} className='hover:scale-105 transition-all h-full' />
+        </div>
+        <div className='flex flex-col gap-1'>
+          <h3 className='font-semibold text-slate-600 text-left capitalize text-2xl md:text-4xl'>{productDisplay.name}</h3>
+          <p className='text-left text-slate-500 font-medium text-2xl'>{productDisplay.category}</p>
+          <p className='font-bold md:text-2xl'><span className='text-red-400'>₹</span>{productDisplay.price}</p>
+          <div className='flex gap-3'>
+            <button className='bg-yellow-500 py-1 my-3 rounded hover:bg-yellow-600 min-w-[100px]'>Buy Now</button>
+            <button className='bg-yellow-500 py-1 my-3 rounded hover:bg-yellow-600 min-w-[100px]'>Add Cart</button>
+          </div>
+          <div>
+            <p className='text-slate-500 font-medium'>Description : </p>
+            <p>{productDisplay.description}</p>
+          </div>          
+        </div>
+      </div>
     </div>
   )
 }
