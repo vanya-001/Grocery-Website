e14645a9c3cbb9362d0c408e47be688c344ab929
Vanya Maheshwari <vanimaheshwari19@gmail.com>
2023-07-31 15:08:58 +0530

    Cart Displayed

---
+++ frontend/src/component/CartProduct.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {AiOutlineMinus, AiOutlinePlus, AiTwotoneDelete} from 'react-icons/ai'
+import { useDispatch } from 'react-redux'
+import { deleteCardItem, increaseQty, decreaseQty } from '../redux/productSlice'
+
+const CartProduct = ({id, name, image, category, price, qty, total}) => {
+
+  const dispatch = useDispatch()
+
+  return (
+    
+    <div className='bg-green-200 p-2 flex gap-4 rounded border border-green-400'>
+      <div className='bg-white p-3 rounded overflow-hidden'>
+        <img src={image} alt="Loading" className='h-28 w-40 object-cover'/>
+      </div>
+      <div className='flex flex-col gap-1 w-full'>
+        <div className='flex justify-between'>
+          <h3 className='font-semibold text-green-900 text-left capitalize text-lg md:text-xl'>{name}</h3>
+          <div className='cursor-pointer hover:text-red-700' onClick={()=>dispatch(deleteCardItem(id))}>
+            <AiTwotoneDelete />
+          </div>
+          </div>
+          <p className='text-left text-slate-500 font-medium text-medium '>{category}</p>
+          <p className='font-bold text-base text-green-700'><span className='text-red-400'>₹</span>{price}</p>
+        
+            <div className='flex justify-between '>
+                <div className='flex gap-3 items-center'>
+                <button onClick={() => dispatch(increaseQty(id))} className='bg-green-400 py-1 mt-1 rounded hover:bg-green-500 p-1'><AiOutlinePlus/></button>
+                <p className='font-semibold p-1'>{qty}</p>
+                <button onClick={()=> dispatch(decreaseQty(id))} className='bg-green-400 py-1 mt-1 rounded hover:bg-green-500 p-1' ><AiOutlineMinus/></button>
+                </div>
+                <div className='flex items-center gap-2 font-bold '>
+                    <p>Total : </p>
+                    <p>{total}</p>
+                </div>
+            </div>
+        </div>    
+    </div>
+  )
+}
+
+export default CartProduct
--- frontend/src/page/Cart.js
+++ frontend/src/page/Cart.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import CartProduct from '../component/CartProduct'
 
 const Cart = () => {
   const productCartItem = useSelector((state) =>state.product.cartItem)
   console.log(productCartItem)
-  
+
   return (
     <div className='p-2 md:p-4'>
       <h2 className='text-lg md:text-2xl font-bold text-green-500'>Your Cart</h2>
-      <div className=''>
+      <div className='my-4'>
         {/* Your Display Cart */}
-        <div className=''>
-            {/* Total Cart Items */}
+        <div className='w-full max-w-3xl'>
+              {
+                productCartItem.map(el => {
+                  return(
+                    <CartProduct  
+                      key={el._id}
+                      id = {el._id}
+                      name = {el.name}
+                      image = {el.image}
+                      category = {el.category} 
+                      qty = {el.qty}
+                      total = {el.total}
+                      price={el.price}
+                    />
+                  )
+                })
+              }            
         </div>
+        {/* Total Cart Items */}
         <div className=''></div>
       </div>
     </div>
